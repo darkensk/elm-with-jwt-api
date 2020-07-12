6dc8c30d1d1ee1cee1f090b22055cacda1e8c143
Polka Oliver <polka.oliver@gmail.com>
2020-07-12 12:43:22 +0200

    Upgrade elm, upgrade packages and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,77 @@
-var gulp = require('gulp');
-var elm = require('gulp-elm');
-var gutil = require('gulp-util');
-var plumber = require('gulp-plumber');
-var connect = require('gulp-connect');
+const gulp = require("gulp");
+const elm = require("gulp-elm");
+const gutil = require("gulp-util");
+// var plumber = require("gulp-plumber");
+const connect = require("gulp-connect");
 
 // File paths
 var paths = {
-  dest: 'dist',
-  elm: 'src/*.elm',
-  static: 'src/*.{html,css}'
+  dest: "dist",
+  elm: "src/*.elm",
+  static: "src/*.{html,css}",
 };
 
-// Init Elm
-gulp.task('elm-init', elm.init);
- 
 // Compile Elm
-gulp.task('elm', ['elm-init'], function(){
-    return gulp.src(paths.elm)
-        .pipe(plumber())
-        .pipe(elm())
-        .pipe(gulp.dest(paths.dest));
-});
+function compileElm() {
+  return gulp
+    .src(paths.elm)
+    .pipe(elm({ optimize: true }))
+    .pipe(gulp.dest(paths.dest));
+}
+// gulp.task("elm", function () {
+//   return (
+//     gulp
+//       .src(paths.elm)
+//       // .pipe(plumber())
+//       .pipe(elm({ optimize: true }))
+//       .pipe(gulp.dest(paths.dest))
+//   );
+// });
 
 // Move static assets to dist
-gulp.task('static', function() {
-    return gulp.src(paths.static)
-        .pipe(plumber())
-        .pipe(gulp.dest(paths.dest));
-});
+function prepareStatic() {
+  return gulp.src(paths.static).pipe(gulp.dest(paths.dest));
+}
+// gulp.task("static", function () {
+//   return (
+//     gulp
+//       .src(paths.static)
+//       .pipe(plumber())
+//       //
+//       .pipe(gulp.dest(paths.dest))
+//   );
+// });
 
 // Watch for changes and compile
-gulp.task('watch', function() {
-    gulp.watch(paths.elm, ['elm']);
-    gulp.watch(paths.static, ['static']);
-});
+function watchFiles() {
+  gulp.watch(paths.elm, compileElm);
+  gulp.watch(paths.static, prepareStatic);
+}
+// gulp.task("watch", function () {
+//   gulp.watch(paths.elm, ["elm"]);
+//   gulp.watch(paths.static, ["static"]);
+// });
 
 // Local server
-gulp.task('connect', function() {
-    connect.server({
-        root: 'dist',
-        port: 3000
-    });
-});
+function connectServer() {
+  return connect.server({ root: "dist", port: 3000 });
+}
+// gulp.task("connect", function () {
+//   connect.server({
+//     root: "dist",
+//     port: 3000,
+//   });
+// });
 
 // Main gulp tasks
-gulp.task('build', ['elm', 'static']);
-gulp.task('default', ['connect', 'build', 'watch']);
+// gulp.task("build", ["elm", "static"]);
+// gulp.task("default", ["connect", "build", "watch"]);
+
+// Define main gulp tasks
+const buildElm = gulp.series(compileElm, prepareStatic);
+
+const buildDefault = gulp.parallel(connectServer, buildElm, watchFiles);
+
+// expose tasks to CLI
+exports.build = buildElm;
+exports.default = buildDefault;
